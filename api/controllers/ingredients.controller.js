88fb21e2d41c8485c07a4fcfeb017d0bd8ae38a3
WhyBecause <maxence.traina@gmail.com>
2021-07-14 15:26:28 +0200

    Log API requests

--- api/controllers/ingredients.controller.js
+++ api/controllers/ingredients.controller.js
@@ -1,9 +1,9 @@
-const db = require("../models")
-const Ingredient = db.ingredient
+const db = require("../models");
+const Ingredient = db.ingredient;
 
 exports.addIngredient = async (req, res) => {
-    let ingredient = new Ingredient(req.body)
-    let result
+    let ingredient = new Ingredient(req.body);
+    let result;
 
     ingredient.validate(async (err) => {
         if (err) {
@@ -11,62 +11,70 @@ exports.addIngredient = async (req, res) => {
             return res.status(422).json(err._message)
         }
         try {
-            ingredient.date = new Date().toISOString()
+            ingredient.date = new Date().toISOString();
             result = await ingredient.save()
-            res.status(200).json(result)
-        } catch(e) {
-            res.status(400).json(e)
+            console.log("Ingredient added");
+            res.status(200).json(result);
+        } catch (e) {
+            console.log(e);
+            res.status(400).json(e);
         }
     })
 }
 
 exports.getIngredients = async (req, res) => {
     try {
-        const ingredients = await Ingredient.find({}).sort({ _id: -1})
-        res.status(200).json(ingredients)
-    } catch(e) {
-        res.status(404).json(e)
+        const ingredients = await Ingredient.find({}).sort({ _id: -1 });
+        console.log("All ingredients fetched");
+        res.status(200).json(ingredients);
+    } catch (e) {
+        console.log(e);
+        res.status(404).json(e);
     }
 }
 
 exports.updateIngredient = async (req, res) => {
-    const { name, quantity } = req.body
-    let ingredient
-    let updatedIngredient
+    const { name, quantity } = req.body;
+    let ingredient;
+    let updatedIngredient;
 
     try {
-        ingredient = await Ingredient.findOne({ _id: req.params.id})
-        if (!ingredient) return res.status(404).json({})
+        ingredient = await Ingredient.findOne({ _id: req.params.id });
+        if (!ingredient) return res.status(404).json({});
 
-        if (name) ingredient.name = name
-        if (quantity || quantity == 0) ingredient.quantity = quantity
+        if (name) ingredient.name = name;
+        if (quantity || quantity == 0) ingredient.quantity = quantity;
 
-        updatedIngredient = await ingredient.save()
-        res.status(200).json(updatedIngredient)
+        updatedIngredient = await ingredient.save();
+        console.log("Ingredient updated");
+        res.status(200).json(updatedIngredient);
     } catch (e) {
         console.log(e.message);
-        res.status(400).json(e.message)
+        res.status(400).json(e.message);
     }
 }
 
 exports.deleteIngredient = async (req, res) => {
     try {
-        const result = await Ingredient.deleteOne({ _id: req.params.id })
+        const result = await Ingredient.deleteOne({ _id: req.params.id });
         if (result.n > 0) {
-            res.status(200).json({message: "Deleted"})
+            console.log("Ingredient deleted");
+            res.status(200).json({ message: "Deleted" });
         } else {
             res.status(401).json("Error")
         }
-    } catch(e) {
-        res.status(400).json(e)
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
     }
 }
 
 exports.deleteAllIngredients = async (req, res) => {
     try {
-        await Ingredient.deleteMany({})
-        res.status(200).json({message: "All ingredients have been deleted"})
-    } catch(e) {
-        res.status(400).json(e)
+        await Ingredient.deleteMany({});
+        res.status(200).json({ message: "All ingredients have been deleted" });
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
     }
 }
